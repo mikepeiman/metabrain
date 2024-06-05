b09be3817721c84d49503f55c7858b3bb08c9d32
Mike Peiman <mikepeiman@gmail.com>
2024-06-05 19:55:50 -0400

    removed js code for global shortcuts, not yet working

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
 	import '../app.css';
-
+	import { register } from '@tauri-apps/plugin-global-shortcut';
+	// await register('CommandOrControl+Shift+C', (event) => {
+	// 	console.log(`🚀 ~ awaitregister ~ event:`, event)
+	// 	if (event.state === 'Pressed') {
+	// 		console.log('Shortcut triggered');
+	// 	}
+	// });
 </script>
 
 <slot></slot>
