2f4970ba7c1abb7357af538b84010479bc5f24e3
Mike Peiman <mikepeiman@gmail.com>
2024-06-05 18:46:43 -0400

    updated Tauri to beta-2.0.0 and ran successful build, however app crashes on launch

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,31 @@
 
 fn main() {
   tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+      .setup(|app| {
+          #[cfg(desktop)]
+          {
+              use tauri::Manager;
+              use tauri_plugin_global_shortcut::{Code, Modifiers, ShortcutState};
+
+              app.handle().plugin(
+                  tauri_plugin_global_shortcut::Builder::new()
+                      .with_shortcuts(["ctrl+d", "alt+space"])?
+                      .with_handler(|app, shortcut, event| {
+                          if event.state == ShortcutState::Pressed  {
+                              if shortcut.matches(Modifiers::CONTROL, Code::KeyD) {
+                                  let _ = app.emit("shortcut-event", "Ctrl+D triggered");
+                              }
+                              if shortcut.matches(Modifiers::ALT, Code::Space) {
+                                  let _ = app.emit("shortcut-event", "Alt+Space triggered");
+                              }
+                          }
+                      })
+                      .build(),
+              )?;
+          }
+
+          Ok(())
+      })
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,7 @@ const config = {
 		adapter: adapter(),
 		alias: {
 			"@/*": "./src/lib/*",
+			"images/*": "./static/images/*",
 		  },
 	}
 };
