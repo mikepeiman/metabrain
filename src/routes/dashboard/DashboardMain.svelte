0f8946fd882a3711aa50ea2ec0225d43e359b904
Mike Peiman <mikepeiman@gmail.com>
2024-06-06 10:53:03 -0400

    home button now returns home

--- src/routes/dashboard/DashboardMain.svelte
+++ src/routes/dashboard/DashboardMain.svelte
@@ -1,41 +1,22 @@
 <script lang="ts">
-  import DashboardHeader from './DashboardHeader.svelte';
-
-	import SideMenuLeft from './SideMenuLeft.svelte';
 
 	import ChevronLeft from 'lucide-svelte/icons/chevron-left';
 	import ChevronRight from 'lucide-svelte/icons/chevron-right';
 	import Copy from 'lucide-svelte/icons/copy';
 	import CreditCard from 'lucide-svelte/icons/credit-card';
 	import File from 'lucide-svelte/icons/file';
-	import Home from 'lucide-svelte/icons/home';
-	import LineChart from 'lucide-svelte/icons/line-chart';
 	import ListFilter from 'lucide-svelte/icons/list-filter';
 	import EllipsisVertical from 'lucide-svelte/icons/ellipsis-vertical';
-	import Package from 'lucide-svelte/icons/package';
-	import Package2 from 'lucide-svelte/icons/package-2';
-	import PanelLeft from 'lucide-svelte/icons/panel-left';
-	import Search from 'lucide-svelte/icons/search';
-	import Settings from 'lucide-svelte/icons/settings';
-	import ShoppingCart from 'lucide-svelte/icons/shopping-cart';
 	import Truck from 'lucide-svelte/icons/truck';
-	import UsersRound from 'lucide-svelte/icons/users-round';
-
 	import { Badge } from '$lib/components/ui/badge/index.js';
-	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-	import { Input } from '$lib/components/ui/input/index.js';
 	import * as Pagination from '$lib/components/ui/pagination/index.js';
 	import { Progress } from '$lib/components/ui/progress/index.js';
 	import { Separator } from '$lib/components/ui/separator/index.js';
-	import * as Sheet from '$lib/components/ui/sheet/index.js';
 	import * as Table from '$lib/components/ui/table/index.js';
 	import * as Tabs from '$lib/components/ui/tabs/index.js';
-	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
-
-	import placeholderImg from '$images/placeholder.jpg';
 </script>
 
 		<main
--- src/routes/dashboard/SideMenuLeft.svelte
+++ src/routes/dashboard/SideMenuLeft.svelte
@@ -1,37 +1,13 @@
 <script lang="ts">
-    import ChevronLeft from "lucide-svelte/icons/chevron-left";
-    import ChevronRight from "lucide-svelte/icons/chevron-right";
-    import Copy from "lucide-svelte/icons/copy";
-    import CreditCard from "lucide-svelte/icons/credit-card";
-    import File from "lucide-svelte/icons/file";
     import Home from "lucide-svelte/icons/home";
     import LineChart from "lucide-svelte/icons/line-chart";
-    import ListFilter from "lucide-svelte/icons/list-filter";
-    import EllipsisVertical from "lucide-svelte/icons/ellipsis-vertical";
     import Package from "lucide-svelte/icons/package";
     import Package2 from "lucide-svelte/icons/package-2";
-    import PanelLeft from "lucide-svelte/icons/panel-left";
-    import Search from "lucide-svelte/icons/search";
     import Settings from "lucide-svelte/icons/settings";
     import ShoppingCart from "lucide-svelte/icons/shopping-cart";
-    import Truck from "lucide-svelte/icons/truck";
     import UsersRound from "lucide-svelte/icons/users-round";
-  
-    import { Badge } from "$lib/components/ui/badge/index.js";
-    import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
-    import { Button } from "$lib/components/ui/button/index.js";
-    import * as Card from "$lib/components/ui/card/index.js";
-    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
-    import { Input } from "$lib/components/ui/input/index.js";
-    import * as Pagination from "$lib/components/ui/pagination/index.js";
-    import { Progress } from "$lib/components/ui/progress/index.js";
-    import { Separator } from "$lib/components/ui/separator/index.js";
-    import * as Sheet from "$lib/components/ui/sheet/index.js";
-    import * as Table from "$lib/components/ui/table/index.js";
-    import * as Tabs from "$lib/components/ui/tabs/index.js";
     import * as Tooltip from "$lib/components/ui/tooltip/index.js";
     
-   import placeholderImg from "$images/placeholder.jpg";
   </script>
 
     <aside class="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
@@ -46,13 +22,13 @@
         <Tooltip.Root>
           <Tooltip.Trigger asChild let:builder>
             <a
-              href="##"
+              href="/"
               class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
               use:builder.action
               {...builder}
             >
-              <Home class="h-5 w-5" />
-              <span class="sr-only">Dashboard</span>
+              <Home class="h-5 w-5"  />
+              <span class="sr-only">Home</span>
             </a>
           </Tooltip.Trigger>
           <Tooltip.Content side="right">Dashboard</Tooltip.Content>
