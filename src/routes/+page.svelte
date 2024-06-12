665faa4c5e07ed48225cc871a786b8a4d7e13ca0
Mike Peiman <mikepeiman@gmail.com>
2024-06-11 20:57:18 -0400

    rolled back to Svelte V4 to avoid package conflicts. Initialized Docker, which still needs work to run

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,31 @@
 <script lang="ts">
     import * as Resizable from "$lib/components/ui/resizable/index.js";
     import CommandPalette from "$lib/components/CommandPalette.svelte";
+    import Button from "$lib/components/ui/button/button.svelte";
   </script>
-   
-  <Resizable.PaneGroup direction="horizontal" class="w-full h-screen rounded-lg border">
-    <Resizable.Pane defaultSize={120}>
-      <div class="flex h-[200px] items-center justify-center p-6 bg-slate-600">
-        <span class="font-semibold">One</span>
-      </div>
-    </Resizable.Pane>
-    <Resizable.Handle />
-    <Resizable.Pane defaultSize={500}>
-      <Resizable.PaneGroup direction="vertical">
-        <Resizable.Pane defaultSize={250}>
-          <div class="flex h-full items-center justify-center p-6">
-            <CommandPalette />
-          </div>
-        </Resizable.Pane>
-        <Resizable.Handle />
-        <Resizable.Pane defaultSize={275}>
-          <div class="flex h-full items-center justify-center p-6">
-            <span class="font-semibold">Three</span>
-          </div>
-        </Resizable.Pane>
-      </Resizable.PaneGroup>
-    </Resizable.Pane>
-  </Resizable.PaneGroup>
-  
+   <div class="flex flex-col w-screen">
+     
+     <CommandPalette />
+       <Resizable.PaneGroup direction="horizontal" class="w-full h-screen rounded-lg border">
+      <Resizable.Pane defaultSize={120}>
+        <div class="flex h-[200px] items-center justify-center p-6 bg-slate-600">
+          <Button href="/dashboard">Dashboard</Button>
+        </div>
+      </Resizable.Pane>
+      <Resizable.Handle />
+      <Resizable.Pane defaultSize={500}>
+        <Resizable.PaneGroup direction="vertical">
+          <Resizable.Pane defaultSize={250}>
+            <div class="flex h-full items-center justify-center p-6">
+            </div>
+          </Resizable.Pane>
+          <Resizable.Handle />
+          <Resizable.Pane defaultSize={275}>
+            <div class="flex h-full items-center justify-center p-6">
+              <span class="font-semibold">Three</span>
+            </div>
+          </Resizable.Pane>
+        </Resizable.PaneGroup>
+      </Resizable.Pane>
+       </Resizable.PaneGroup>
+   </div>
