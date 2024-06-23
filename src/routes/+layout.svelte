23e29d4acac5b71856f51cd75118620c08ddc511
Mike Peiman <mikepeiman@gmail.com>
2024-06-23 08:44:51 -0400

    installed pb client and began adding scripts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,22 @@
 	// 		console.log('Shortcut triggered');
 	// 	}
 	// });
+
+	import PocketBase from 'pocketbase'
+
+const pb = new PocketBase('http://127.0.0.1:8090')
+
+
+// list and filter "example" collection records
+const result = await pb.collection('notes').getList(1, 20, {
+    filter: 'status = true && created > "2022-08-01 10:00:00"'
+});
+
+// authenticate as auth collection record
+const userData = await pb.collection('users').authWithPassword('test@example.com', '123456');
+
+// or as super-admin
+const adminData = await pb.admins.authWithPassword('test@example.com', '123456');
 </script>
 
 <div class="flex min-h-screen w-full items-center justify-center bg-cyan-950">
