e746bd9f0f4ff3475812727ce66ce6484fdbbc2e
Mike Peiman <mikepeiman@gmail.com>
2024-07-10 08:12:45 -0400

    cleaned up notes route

--- src/routes/notes/+page.svelte
+++ src/routes/notes/+page.svelte
@@ -1,37 +1,10 @@
 <script lang="ts">
-	import * as Resizable from '$lib/components/ui/resizable/index.js';
-	import CommandPalette from '$lib/components/CommandPalette.svelte';
-	import Button from '$lib/components/ui/button/button.svelte';
-	// import Notes from '$lib/components/Notes.svelte';
-	import PocketBase from 'pocketbase';
-	import { onMount } from 'svelte';
+
 	import MilkdownNotes from '@/components/MilkdownNotes.svelte';
 </script>
 
 <div class="flex w-screen flex-col">
-	<!-- <Notes /> -->
+
   <MilkdownNotes />
- <CommandPalette />
-<!--  <Resizable.PaneGroup direction="horizontal" class="w-full h-screen rounded-lg border">
-      <Resizable.Pane defaultSize={120}>
-        <div class="flex h-[200px] items-center justify-center p-6 bg-slate-600">
-          <Button href="/dashboard">Dashboard</Button>
-        </div>
-      </Resizable.Pane>
-      <Resizable.Handle />
-      <Resizable.Pane defaultSize={500}>
-        <Resizable.PaneGroup direction="vertical">
-          <Resizable.Pane defaultSize={250}>
-            <div class="flex h-full items-center justify-center p-6">
-            </div>
-          </Resizable.Pane>
-          <Resizable.Handle />
-          <Resizable.Pane defaultSize={275}>
-            <div class="flex h-full items-center justify-center p-6">
-              <span class="font-semibold">Three</span>
-            </div>
-          </Resizable.Pane>
-        </Resizable.PaneGroup>
-      </Resizable.Pane>
-       </Resizable.PaneGroup> -->
+
 </div>
